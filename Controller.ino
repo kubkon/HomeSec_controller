dbacc93c31edf90166def1607a6d41db2fea1a3a
Jakub Konka <kubkon@jakubkonka.com>
2017-03-18 21:47:10 +0000

    Connect to WiFi with MKR1000; hide WiFi credentials

--- Controller.ino
+++ Controller.ino
@@ -1,99 +1,46 @@
 #include <SPI.h>
-#include <Ethernet.h>
-#include <EthernetUDP.h>
+#include <WiFi101.h>
+#include "credentials.h"
 
-// Ethernet vars
-byte mac[] = {
-  0x90, 0xA2, 0xDA, 0x0F, 0x7E, 0xFA
-};
-EthernetClient client;
-
-EthernetUDP udp;
-IPAddress remote(192,168,0,253); // address of the server
-unsigned int local_port = 8888;
-char output_buffer[UDP_TX_PACKET_MAX_SIZE];
-
-// PIR vars
-int pir_pin = 2;
-int pir_state = LOW;
-int pir_value = 0;
-
-// Reed switch vars
-int reed_pin = A0;
-int reed_value = 0;
+// WiFi vars
+int wifi_status = WL_IDLE_STATUS;
 
 void setup() {
   // wait for serial; for debugging only
   Serial.begin(9600);
-  while(!Serial) {;}
-
-  // set up the Ethernet
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP!");
-    for (;;) {;}
-    // TODO try static IP
-    // otherwise, lit up an LED to denote the status and
-    // request hard reboot
+  while(!Serial) {
+    ; // wait for serial for connect
   }
-  // and UDP
-  udp.begin(local_port);
 
-  // set up the PIR sensor
-  pinMode(pir_pin, INPUT);
+  // attempt to connect to WiFi network
+  while (wifi_status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    wifi_status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
 }
 
 void loop() {
-  // maintain DHCP lease
-  maintain_dhcp();
-
-  pir_value = digitalRead(pir_pin);   // read input value
-  if (pir_value == HIGH) {            // check if the input is HIGH
-    if (pir_state == LOW) {
-      // we have just turned on
-      Serial.println("Motion detected!");
-      publish_trigger();
-      pir_state = HIGH;
-    }
-  } else {
-    if (pir_state == HIGH){
-      // we have just turned of
-      Serial.println("Motion ended!");
-      pir_state = LOW;
-    }
-  }
-
-  reed_value = analogRead(reed_pin);
-  if (reed_value == 0) {
-    Serial.println("Door was open!");
-    publish_trigger();
-  }
+  printWiFiStatus();
 }
 
-void maintain_dhcp() {
-  switch(Ethernet.maintain()) {
-    case 1:
-    case 3:
-      // TODO handle DHCP failure
-      Serial.println("Error: DHCP failure");
-      break;
-    case 2:
-    case 4:
-    default:
-      break; 
-  }
-}
-
-void publish_trigger() {
-    output_buffer[0] = 'A';
-    output_buffer[1] = 'L';
-    output_buffer[2] = 'E';
-    output_buffer[3] = 'R';
-    output_buffer[4] = 'T';
-
-    udp.beginPacket(remote, local_port);
-    udp.write(output_buffer, UDP_TX_PACKET_MAX_SIZE);
-    udp.endPacket();
-
-    memset(output_buffer, 0, UDP_TX_PACKET_MAX_SIZE);
+void printWiFiStatus() {
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
 
